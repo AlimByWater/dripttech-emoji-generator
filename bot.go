4a9c624f2d965ff20f7b61588e5a5294db1965d0
a.tsagaraev <a.tsagaraev@mtlab.space>
2024-12-06 17:44:52 +0300

    name + dm commnds

--- bot.go
+++ bot.go
@@ -76,7 +76,6 @@ func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 
 		if strings.HasPrefix(update.Message.Text, "/emoji") ||
 			strings.HasPrefix(update.Message.Caption, "/emoji ") {
-
 			handleEmojiCommandForDM(ctx, b, update)
 			return
 		}
@@ -95,6 +94,140 @@ func handleEmojiCommandForDM(ctx context.Context, b *bot.Bot, update *models.Upd
 		sendMessageByBot(ctx, b, update, "Вы не можете создавать паки в личном чате. Возможно когда-нибудь...")
 		return
 	}
+
+	// Extract command arguments
+	args := extractCommandArgs(update.Message)
+	emojiArgs, err := parseArgs(args)
+	if err != nil {
+		slog.Error("Invalid arguments", slog.String("err", err.Error()))
+		sendMessageByBot(ctx, b, update, err.Error())
+		return
+	}
+
+	if update.Message.From.IsBot || update.Message.From.ID < 0 {
+		sendMessageByBot(ctx, b, update, "Создать пак можно только с личного аккаунта")
+		return
+	}
+
+	// Setup command defaults and working environment
+	setupEmojiCommand(emojiArgs, update.Message)
+
+	// Get bot info and setup pack details
+	botInfo, err := b.GetMe(ctx)
+	if err != nil {
+		slog.Error("Failed to get bot info", slog.String("err", err.Error()))
+		sendMessageByBot(ctx, b, update, "Не удалось получить информацию о боте")
+		return
+	}
+
+	emojiPack, err := setupPackDetails(ctx, emojiArgs, botInfo)
+	if err != nil {
+		slog.Error("Failed to setup pack details", slog.String("err", err.Error()))
+		sendMessageByBot(ctx, b, update, "пак с подобной ссылкой не найден")
+		return
+	}
+
+	// Create working directory and download file
+	if err := prepareWorkingEnvironment(ctx, b, update, emojiArgs); err != nil {
+		slog.Error("Failed to download file", slog.String("err", err.Error()))
+		var message string
+		switch err {
+		case types.ErrFileNotProvided:
+			message = "Нужен файл для создания эмодзи"
+		case types.ErrFileOfInvalidType:
+			message = "Неподдерживаемый тип файла. Поддерживаются: GIF, JPEG, PNG, WebP, MP4, WebM, MPEG"
+		case types.ErrGetFileFromTelegram:
+			message = "Не удалось получить файл из Telegram"
+		case types.ErrFileDownloadFailed:
+			message = "Ошибка при загрузке файла"
+		default:
+			message = "Ошибка при загрузке файла"
+		}
+		sendMessageByBot(ctx, b, update, message)
+		return
+	}
+
+	if emojiPack == nil {
+		// Create database record
+		emojiPack, err = createDatabaseRecord(ctx, emojiArgs, args, botInfo.Username)
+		if err != nil {
+			slog.Error("Failed to log emoji command",
+				slog.String("err", err.Error()),
+				slog.String("pack_link", emojiArgs.PackLink),
+				slog.Int64("user_id", emojiArgs.UserID))
+			sendMessageByBot(ctx, b, update, "Не удалось создать запись в базе данных")
+			return
+		}
+	}
+
+	var stickerSet *models.StickerSet
+
+	for {
+		// Обрабатываем видео
+		createdFiles, err := processVideo(emojiArgs)
+		if err != nil {
+			slog.LogAttrs(ctx, slog.LevelError, "Ошибка при обработке видео", emojiArgs.ToSlogAttributes(slog.String("err", err.Error()))...)
+			err2 := removeDirectory(emojiArgs.WorkingDir)
+			if err2 != nil {
+				slog.Error("Failed to remove directory", slog.String("err", err2.Error()), slog.String("dir", emojiArgs.WorkingDir), slog.String("emojiPackLink", emojiArgs.PackLink), slog.Int64("user_id", emojiArgs.UserID))
+			}
+			sendMessageByBot(ctx, b, update, fmt.Sprintf("Ошибка при обработке видео: %s", err.Error()))
+			return
+		}
+
+		// Создаем набор стикеров
+		stickerSet, _, err = addEmojis(ctx, b, emojiArgs, createdFiles)
+		if err != nil {
+			if strings.Contains(err.Error(), "PEER_ID_INVALID") || strings.Contains(err.Error(), "user not found") || strings.Contains(err.Error(), "bot was blocked by the user") {
+				sendInitMessage(update.Message.Chat.ID, update.Message.ID)
+				// TODO implement later
+				//messagesToDelete.Store(update.Message.From.ID, update.Message.ID)
+				return
+
+			}
+
+			if strings.Contains(err.Error(), "STICKER_VIDEO_BIG") {
+				emojiArgs.QualityValue++
+				continue
+			}
+
+			if strings.Contains(err.Error(), "STICKERSET_INVALID") {
+				sendMessageByBot(ctx, b, update, fmt.Sprintf("Не получилось создать некоторые эмодзи. Попробуйте еще раз, либо измените файл."))
+				return
+			}
+
+			if strings.Contains(err.Error(), "retry_after") {
+				parts := strings.Split(err.Error(), "retry_after ")
+				var waitTime int
+				if len(parts) >= 2 {
+					if wt, parseErr := strconv.Atoi(strings.TrimSpace(parts[1])); parseErr == nil {
+						waitTime = wt
+					}
+				}
+
+				if waitTime > 0 {
+					dur := time.Duration(waitTime * int(time.Second))
+					sendMessageByBot(ctx, b, update, fmt.Sprintf("Вы сможете создать пак только через %.0f минуты", dur.Minutes()))
+					return
+				}
+			}
+
+			sendMessageByBot(ctx, b, update, fmt.Sprintf("%s", err.Error()))
+			return
+		}
+
+		break
+	}
+
+	// Обновляем количество эмодзи в базе данных
+	if err := db.Postgres.SetEmojiCount(ctx, emojiPack.ID, len(stickerSet.Stickers)); err != nil {
+		slog.Error("Failed to update emoji count",
+			slog.String("err", err.Error()),
+			slog.String("pack_link", emojiArgs.PackLink),
+			slog.Int64("user_id", emojiArgs.UserID))
+	}
+
+	sendMessageByBot(ctx, b, update, fmt.Sprintf("Ваш пак\n%s", "https://t.me/addemoji/"+emojiArgs.PackLink))
 
 }
 
@@ -513,7 +646,8 @@ func setupEmojiCommand(args *types.EmojiCommand, msg *models.Message) {
 		if len(args.SetName) > types.TelegramPackLinkAndNameLength-len(types.PackTitleTempl) {
 			args.SetName = args.SetName[:types.TelegramPackLinkAndNameLength-len(types.PackTitleTempl)]
 		}
-		args.SetName = args.SetName + " " + types.PackTitleTempl
+		args.SetName = fmt.Sprintf(`%s
+%s`, args.SetName, types.PackTitleTempl)
 	}
 
 	// Setup working directory and user info
